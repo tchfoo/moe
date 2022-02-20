0ebd9121d7ed92dcb79823eb7a498832ff6b8a89
YMSTNT <21342713+YMSTNT@users.noreply.github.com>
2022-02-20 16:45:18 +0100

    Added Emote.cs to define global emotes

---
+++ Emotes.cs
@@ -0,0 +1,11 @@
+using Discord;
+
+namespace TNTBot
+{
+  class Emotes
+  {
+    public static Emoji SuccessEmote = new Emoji("\u2705");
+
+    public static Emoji ErrorEmote = new Emoji("\u274C");
+  }
+}
