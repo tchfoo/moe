d07d8cd0dd419c5c520d0a8e8aa45e09a141fe6b
Gutyina Gergő <gutyina.gergo.2@gmail.com>
2022-02-21 20:04:55 +0100

    Settings: Refactor use custom color class

--- Commands/SettingsCommand.cs
+++ Commands/SettingsCommand.cs
@@ -1,7 +1,6 @@
 using Discord;
 using Discord.WebSocket;
 using TNTBot.Services;
-using System.Drawing;
 
 namespace TNTBot.Commands
 {
@@ -59,15 +58,13 @@ namespace TNTBot.Commands
       var pinChannel = await service.GetPinChannel(guild);
       var logChannel = await service.GetLogChannel(guild);
 
-      System.Drawing.Color blurple = ColorTranslator.FromHtml("#7289DA");
-
       var embed = new EmbedBuilder()
         .WithAuthor(guild.Name, iconUrl: guild.IconUrl)
         .WithTitle("Bot Settings")
         .AddField("Mute length", muteLength, inline: true)
         .AddField("Pin chanel", pinChannel?.Mention ?? "None", inline: true)
         .AddField("Log channel", logChannel?.Mention ?? "None", inline: true)
-        .WithColor((Discord.Color)blurple);
+        .WithColor(Colors.Blurple);
 
       await cmd.RespondAsync(embed: embed.Build());
     }
