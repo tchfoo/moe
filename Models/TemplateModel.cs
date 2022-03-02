76568921181a2b85b93796bd1c72146eef63a119
Gutyina Gergő <gutyina.gergo.2@gmail.com>
2022-03-02 15:55:11 +0100

    Template: Add model

---
+++ Models/TemplateModel.cs
@@ -0,0 +1,22 @@
+using Discord.WebSocket;
+
+namespace TNTBot.Models
+{
+  public class TemplateModel
+  {
+    public int Id { get; set; }
+    public SocketGuild Guild { get; set; } = default!;
+    public string Name { get; set; } = default!;
+    public bool Hidden { get; set; }
+    public SocketGuildUser Creator { get; set; } = default!;
+    public SocketTextChannel Channel { get; set; } = default!;
+    public SocketRole? MentionedRole { get; set; }
+    public string? AuthorName { get; set; }
+    public string? AuthorImageUrl { get; set; }
+    public string? ThumbnailImageUrl { get; set; }
+    public string? ImageUrl { get; set; }
+    public string Title { get; set; } = default!;
+    public string Description { get; set; } = default!;
+    public string? Footer { get; set; }
+  }
+}
