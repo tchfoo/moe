tchfoo/moe

commit 7d3ddb6319585b22c4c998e826dd2a054dc24e07

Refactor: Remove HasSomething methods for less DB calls (closes #8) Rename ListSomethings to GetSomethings in services Prefer x.Count == 0 over !x.Any() Prefer x.ToList() over x.ToArray()

Services/SnapshotService.cs
@@ -21,13 +21,6 @@ public class SnapshotService
     return settingsService.IsAuthorized(user, requiredLevel, out error);
   }
 
-  public async Task<bool> HasSnapshots(SocketGuild guild)
-  {
-    var sql = "SELECT COUNT(*) FROM snapshots WHERE guild_id = $0";
-    var count = await DatabaseService.QueryFirst<int>(sql, guild.Id);
-    return count > 0;
-  }
-
   public async Task<bool> HasSnapshot(SocketGuild guild, string name)
   {
     var sql = "SELECT COUNT(*) FROM snapshots WHERE guild_id = $0 AND name = $1";
@@ -35,7 +28,7 @@ public class SnapshotService
     return count > 0;
   }
 
-  public async Task<List<string>> ListSnapshots(SocketGuild guild)
+  public async Task<List<string>> GetSnapshots(SocketGuild guild)
   {
     var sql = "SELECT name FROM snapshots WHERE guild_id = $0";
     var snapshots = await DatabaseService.Query<string>(sql, guild.Id);
@@ -262,7 +255,7 @@ public class SnapshotService
       sql = $"INSERT INTO snapshot_channels(snapshot_id, channel_id, channel_name) VALUES {placeholders}";
       var values = s.Channels
         .SelectMany(x => new object[] { id, x.Key, x.Value })
-        .ToArray();
+        .ToList();
       await DatabaseService.NonQuery(sql, values);
     }
 
@@ -273,7 +266,7 @@ public class SnapshotService
       sql = $"INSERT INTO snapshot_roles(snapshot_id, role_id, role_name) VALUES {placeholders}";
       var values = s.Roles
         .SelectMany(x => new object[] { id, x.Key, x.Value })
-        .ToArray();
+        .ToList();
       await DatabaseService.NonQuery(sql, values);
     }
   }

Commands/CustomCommandCommand.cs
@@ -114,13 +114,12 @@ public class CustomCommandCommand : SlashCommandBase
 
   private async Task ListCommands(SocketSlashCommand cmd, SocketGuild guild)
   {
-    if (!await service.HasCommands(guild))
+    var commands = await service.GetCommands(guild);
+    if (commands.Count == 0)
     {
       await cmd.RespondAsync($"{Emotes.ErrorEmote} There are no custom commands");
       return;
     }
-
-    var commands = await service.GetCommands(guild);
 
     var p = new PaginatableEmbedBuilder<CustomCommand>
       (5, commands, items =>

Commands/CustomRoleCommand.cs
@@ -105,15 +105,14 @@ public class CustomRoleCommand : SlashCommandBase
   private async Task SelectRole(SocketSlashCommand cmd, SocketSlashCommandDataOption subcommand, SocketGuild guild)
   {
     var user = (SocketGuildUser)cmd.User;
-
-    if (!await service.HasRoles(guild))
+    var roles = await service.GetRoles(guild);
+    if (roles.Count == 0)
     {
       await cmd.RespondAsync($"{Emotes.ErrorEmote} There are no custom roles");
       return;
     }
 
-    var roles = await service.GetRoles(guild);
-    IEnumerable<CustomRole> oldSubscribedRoles = await service.GetSubscribedRoles(user);
+    var oldSubscribedRoles = await service.GetSubscribedRoles(user);
     var options = roles.Select(x =>
         new SelectMenuOptionBuilder(x.Name, x.Name, x.Description, isDefault: oldSubscribedRoles.Contains(x))
       ).ToList();
@@ -135,7 +134,8 @@ public class CustomRoleCommand : SlashCommandBase
       var newRoles = newSubscribedRoles
         .ExceptBy(oldSubscribedRoles.Select(x => x.DiscordRole), x => x.DiscordRole);
       var tooHighPermRoles = newRoles
-        .Where(x => x.DiscordRole.Position > highestBotRole.Position);
+        .Where(x => x.DiscordRole.Position > highestBotRole.Position)
+        .ToList();
 
       newSubscribedRoles = newSubscribedRoles
         .ExceptBy(tooHighPermRoles.Select(x => x.DiscordRole), x => x.DiscordRole)
@@ -144,7 +144,7 @@ public class CustomRoleCommand : SlashCommandBase
       await service.SyncRoleSubscriptions(user, oldSubscribedRoles, newSubscribedRoles);
       oldSubscribedRoles = newSubscribedRoles;
 
-      if (tooHighPermRoles.Any())
+      if (tooHighPermRoles.Count > 0)
       {
         await submitted.RespondAsync($"{Emotes.ErrorEmote} Some of the roles you have selected can't be applied to due permission issues with Discord roles. This incident will be reported", ephemeral: true);
         foreach (var role in tooHighPermRoles)

Commands/RngCommand.cs
@@ -69,8 +69,10 @@ public class RngCommand : SlashCommandBase
   {
     try
     {
-      var nums = args.Select(int.Parse);
-      if (!nums.Any())
+      var nums = args
+        .Select(int.Parse)
+        .ToList();
+      if (nums.Count == 0)
       {
         throw new FormatException();
       }

Commands/SettingsCommand.cs
@@ -152,7 +152,8 @@ public class SettingsCommand : SlashCommandBase
     string leaveMessageString = leaveMessage is null ? "None" : $"{leaveMessage.Value.Message} in {leaveMessage.Value.Channel.Mention}";
     var timeZone = await service.GetTimeZone(guild);
     var autoroles = (await service.GetAutoroles(guild))
-      .Select(x => x.Mention);
+      .Select(x => x.Mention)
+      .ToList();
 
     var embed = new EmbedBuilder()
       .WithAuthor(guild.Name, iconUrl: guild.IconUrl)
@@ -160,12 +161,12 @@ public class SettingsCommand : SlashCommandBase
       .AddField("Pin chanel", pinChannel?.Mention ?? "None", inline: true)
       .AddField("Log channel", logChannel?.Mention ?? "None", inline: true)
       .AddField("Custom command prefix", prefix, inline: true)
-      .AddField("Bot Administrator ranks", modrankAdmins.Any() ? string.Join(" ", modrankAdmins) : "None", inline: true)
-      .AddField("Moderator ranks", modrankMods.Any() ? string.Join(" ", modrankMods) : "None", inline: true)
+      .AddField("Bot Administrator ranks", modrankAdmins.Count > 0 ? string.Join(" ", modrankAdmins) : "None", inline: true)
+      .AddField("Moderator ranks", modrankMods.Count > 0 ? string.Join(" ", modrankMods) : "None", inline: true)
       .AddField("No-XP role", noXPRole?.Mention ?? "None", inline: true)
       .AddField("Leave message", leaveMessageString, inline: true)
       .AddField("Time zone", timeZone is null ? "None" : timeZone.TimeZoneString, inline: true)
-      .AddField("Autoroles", autoroles.Any() ? string.Join(" ", autoroles) : "None", inline: true)
+      .AddField("Autoroles", autoroles.Count > 0 ? string.Join(" ", autoroles) : "None", inline: true)
       .WithColor(Colors.Blurple);
 
     await cmd.RespondAsync(embed: embed.Build());

Commands/SnapshotCommand.cs
@@ -149,13 +149,13 @@ public class SnapshotCommand : SlashCommandBase
   }
   private async Task ListSnapshots(SocketSlashCommand cmd, SocketGuild guild)
   {
-    if (!await service.HasSnapshots(guild))
+    var snapshots = await service.GetSnapshots(guild);
+    if (snapshots.Count == 0)
     {
       await cmd.RespondAsync($"{Emotes.ErrorEmote} There are no snapshots");
       return;
     }
 
-    var snapshots = await service.ListSnapshots(guild);
 
     var p = new PaginatableEmbedBuilder<string>
       (10, snapshots, items =>

Commands/TemplateCommand.cs
@@ -168,9 +168,8 @@ public class TemplateCommand : SlashCommandBase
   private async Task ListTemplates(SocketSlashCommand cmd, SocketGuildUser user)
   {
     var guild = user.Guild;
-    var templates = await service.ListTemplates(user.Guild);
-
-    if (!await service.HasTemplates(guild))
+    var templates = await service.GetTemplates(user.Guild);
+    if (templates.Count == 0)
     {
       await cmd.RespondAsync($"{Emotes.ErrorEmote} There are no templates");
       return;

Services/CustomCommandService.cs
@@ -19,13 +19,6 @@ public class CustomCommandService
     return settingsService.IsAuthorized(user, requiredLevel, out error);
   }
 
-  public async Task<bool> HasCommands(SocketGuild guild)
-  {
-    var sql = "SELECT COUNT(*) FROM custom_commands WHERE guild_id = $0";
-    var count = await DatabaseService.QueryFirst<int>(sql, guild.Id);
-    return count > 0;
-  }
-
   public async Task<bool> HasCommand(SocketGuild guild, string name)
   {
     name = await CleanCommandName(guild, name);

commit f890e552c8fd034811c3da5e1ec90d5c81c749be

Fix snapshot add crashing
Bug was introduced in 7d3ddb6319585b22c4c998e826dd2a054dc24e07.

Services/SnapshotService.cs
@@ -255,7 +255,7 @@ public class SnapshotService
       sql = $"INSERT INTO snapshot_channels(snapshot_id, channel_id, channel_name) VALUES {placeholders}";
       var values = s.Channels
         .SelectMany(x => new object[] { id, x.Key, x.Value })
-        .ToList();
+        .ToArray();
       await DatabaseService.NonQuery(sql, values);
     }
 
@@ -266,7 +266,7 @@ public class SnapshotService
       sql = $"INSERT INTO snapshot_roles(snapshot_id, role_id, role_name) VALUES {placeholders}";
       var values = s.Roles
         .SelectMany(x => new object[] { id, x.Key, x.Value })
-        .ToList();
+        .ToArray();
       await DatabaseService.NonQuery(sql, values);
     }
   }