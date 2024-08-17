f71265cda7eee7b7b37a5b0dced526d6a5a2bd76
Gutyina Gergő <gutyina.gergo.2@gmail.com>
2024-08-17 23:12:22 +0200

    Add support for most sites in embed fixer

--- Services/EmbedFixerService.cs
+++ Services/EmbedFixerService.cs
@@ -10,7 +10,13 @@ public class EmbedFixerService
   {
     linkRegexes = new Dictionary<string, string>
     {
-      { "https://reddit.com", "https://rxddit.com" }
+      { @"https?://(?:clips\.twitch\.tv|(?:www\.)?twitch\.tv/\w+\/clip)\/([A-Za-z0-9-_]+)", @"https://clips.fxtwitch.tv/$1"},
+      { @"https?://(?:[\w-]+?\.)?reddit\.com", @"https://rxddit.com" },
+      { @"https?://(?:www\.)?threads\.net", @"https://fixthreads.net" },
+      { @"https?://(?:www\.)?(twitter|x)\.com", @"https://fxtwitter.com" },
+      { @"https?://(?:www\.)?instagram.com", @"https://ddinstagram.com" },
+      { @"https?://(?:to)?github\.com/([A-Za-z0-9-]+/[A-Za-z0-9._-]+)/(?:issues|pull)/([0-9]+)([^\s]*)?", @"[$1#$2$3]($&)" },
+      { @"https?://(?:www\.)?tiktok.com", @"https://vxtiktok.com" },
     };
   }
 
