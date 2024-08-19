a2fd16d8163bbb19d3b07fa4ccf17bfe1099d47e
Gutyina Gergő <gutyina.gergo.2@gmail.com>
2024-08-19 10:31:17 +0200

    Initialize embed fixer table for each guild

--- Services/EmbedFixerService.cs
+++ Services/EmbedFixerService.cs
@@ -4,6 +4,12 @@ namespace Moe.Services;
 
 public class EmbedFixerService
 {
+  // This ensures the user can delete all the regexes without being regenerated
+  // When populating the embed_fixer table for a guild, insert this magic
+  // Only populate when this magic is not set
+  // Should be hidden from the user
+  private static readonly string initializedMagic = "_initialized-634761";
+
   private readonly Dictionary<string, string> linkRegexes;
 
   public EmbedFixerService()
@@ -18,6 +24,8 @@ public class EmbedFixerService
       { @"https?://(?:to)?github\.com/([A-Za-z0-9-]+/[A-Za-z0-9._-]+)/(?:issues|pull)/([0-9]+)([^\s]*)?", @"[$1#$2$3]($&)" },
       { @"https?://(?:www\.)?tiktok.com", @"https://vxtiktok.com" },
     };
+
+    InitializeDatabase().Wait();
   }
 
   public string ReplaceLinks(string input)
@@ -29,5 +37,57 @@ public class EmbedFixerService
     }
 
     return input;
+  }
+
+  private async Task InitializeDatabase()
+  {
+    await CreateEmbedFixerTable();
+
+    var joinedGuilds = DiscordService.Discord.Guilds.Select(x => x.Id);
+    var initializedGuilds = await GetInitializedGuilds();
+    var uninitializedGuilds = joinedGuilds.Except(initializedGuilds);
+
+    foreach (var guildId in uninitializedGuilds)
+    {
+      await PopulateEmbedFixerTable(guildId);
+    }
+  }
+
+  private async Task<List<ulong>> GetInitializedGuilds()
+  {
+    var sql = "SELECT guild_id FROM embed_fixer WHERE pattern = $0";
+    var result = await DatabaseService.Query<ulong>(sql, initializedMagic);
+    if (result == null)
+    {
+      return new();
+    }
+
+    return result;
+  }
+
+  private async Task CreateEmbedFixerTable()
+  {
+    var sql = @"
+      CREATE TABLE IF NOT EXISTS embed_fixer(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        guild_id INTEGER NOT NULL,
+        pattern TEXT NOT NULL,
+        replacement TEXT NOT NULL
+      )";
+    await DatabaseService.NonQuery(sql);
+  }
+
+  private async Task PopulateEmbedFixerTable(ulong guildId)
+  {
+    var sql = @"
+      INSERT INTO embed_fixer (guild_id, pattern, replacement)
+      VALUES($0, $1, $2)";
+
+    await DatabaseService.NonQuery(sql, guildId, initializedMagic, string.Empty);
+
+    foreach (var (pattern, replacement) in linkRegexes)
+    {
+      await DatabaseService.NonQuery(sql, guildId, pattern, replacement);
+    }
   }
 }
