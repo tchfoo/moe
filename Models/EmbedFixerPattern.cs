e11072b5e11192f2d4923662d8915ae2f1dae4b5
Gutyina Gergő <gutyina.gergo.2@gmail.com>
2024-08-21 14:04:47 +0200

    Rename EmbedFixerModel to EmbedFixerPattern

--- Models/EmbedFixerPattern.cs
+++ Models/EmbedFixerPattern.cs
@@ -2,7 +2,7 @@ using Discord.WebSocket;
 
 namespace Moe.Models;
 
-public class EmbedFixerModel
+public class EmbedFixerPattern
 {
   public string Pattern { get; set; } = default!;
   public string Replacement { get; set; } = default!;
