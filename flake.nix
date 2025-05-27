88db15c5551c6eb5f8e242269cca4d3e4bd24247
Gutyina Gergő <gutyina.gergo.2@gmail.com>
2025-05-27 20:40:19 +0200

    flake.lock: Update (nuget-packageslock2nix issue fixed)
    Flake lock file updates:
    
    • Updated input 'flake-utils':
        'github:numtide/flake-utils/b1d9ab70662946ef0850d488da1c9019f3a9752a' (2024-03-11)
      → 'github:numtide/flake-utils/11707dc2f618dd54ca8739b309ec4fc024de578b' (2024-11-13)
    • Updated input 'nixpkgs':
        'github:NixOS/nixpkgs/7402aa90cff52a03f14e680346fa4038a1e17e93' (2024-07-30)
      → 'github:NixOS/nixpkgs/62b852f6c6742134ade1abdd2a21685fd617a291' (2025-05-25)
    • Updated input 'nuget-packageslock2nix':
        'github:mdarocha/nuget-packageslock2nix/5897dae0aaee226bce938afd787ada535a8f0ea2' (2023-07-31)
      → 'github:mdarocha/nuget-packageslock2nix/d483a466e5219674bbecd58320b68f91177f0acf' (2025-05-27)

--- flake.nix
+++ flake.nix
@@ -2,8 +2,7 @@
   description = "A multi-purpose Discord bot made using Discord.Net.";
 
   inputs = {
-    # TODO: unpin when fixed: https://github.com/NixOS/nixpkgs/issues/347310
-    nixpkgs.url = "github:NixOS/nixpkgs/7402aa90cff52a03f14e680346fa4038a1e17e93";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
     nuget-packageslock2nix = {
       url = "github:mdarocha/nuget-packageslock2nix/main";
